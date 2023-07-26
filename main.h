a8d4df4f61dcf7f30d9e77be1a87a3e6943fe5b4
LusterPearl <Salamioluwatosin90@gmail.com>
2023-07-26 11:35:40 -0700

    main.h

--- main.h
+++ main.h
@@ -32,14 +32,15 @@ struct fmt
 };
 
 /**
- * typedef struct fmt fmt_t, - Struct op
+ * typedef struct fmt fmt_t - Struct op
  *
  * @fmt: The format
+ * @fm_t: The function associated
  */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i
+int handle_print(const char *fmt, int *i,
 		va_list list, char buffer[], int flags, int width, int precision,
 		int size);
 
@@ -61,7 +62,7 @@ int print_binary(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
-int print_octa(va_list types, char buffer[],
+int print_octal(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
@@ -70,7 +71,7 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 		char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function tp print non printable characters */
+/* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
@@ -114,4 +115,3 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
-
